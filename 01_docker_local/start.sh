6c6ebd01af700b765c18120f51d2a87d9c5003e6
Juan Miguel Martin Cabello <jmarcab01@gmail.com>
2019-10-06 11:43:55 +0000

    start.sh error

--- 01_docker_local/start.sh
+++ 01_docker_local/start.sh
@@ -8,17 +8,16 @@ docker build -t flaskblog .
 docker network create dev
 
 #Reading database params
-config=`cat blog/config.py | grep MYSQL`
-password=`echo $config | grep MYSQL_DATABASE_PASSWORD | sed 's/MYSQL_DATABASE_PASSWORD = //g'`
-user=`echo $config | grep MYSQL_DATABASE_USER | sed 's/MYSQL_DATABASE_USER = //g'`
-host=`echo $config | grep MYSQL_DATABASE_HOST | sed 's/MYSQL_DATABASE_HOST = //g'`
+cat blog/config.py | grep MYSQL | sed "s/'//g" > db.txt
+password=`grep MYSQL_DATABASE_PASSWORD < db.txt | \
+ sed 's/MYSQL_DATABASE_PASSWORD = //g'`
+user=`grep MYSQL_DATABASE_USER < db.txt | \
+ sed 's/MYSQL_DATABASE_USER = //g'`
+host=`grep MYSQL_DATABASE_HOST < db.txt | \
+ sed 's/MYSQL_DATABASE_HOST = //g'`
 
 #Creating database
-docker run -d --name sql -v `pwd`/bbdd:/docker-entrypoint-initdb.d/ \ 
--e MYSQL_ROOT_PASSWORD=$password \
--e MYSQL_ROOT_USER=$user \
--e MYSQL_ROOT_HOST=$host \
---network dev percona:latest
+docker run -d --name sql -v `pwd`/bbdd:/docker-entrypoint-initdb.d/ -e MYSQL_ROOT_PASSWORD=$password -e MYSQL_ROOT_USER=$user -e MYSQL_ROOT_HOST=$host --network dev percona:latest
 
 #Running app container
 docker run -d -p 80:5000 --network dev --name app flaskblog
